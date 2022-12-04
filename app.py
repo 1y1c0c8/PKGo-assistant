cfeefc7c2e9cb942d7028e5d84d5cec82e9fb399
1y1c0c8 <f74096255@gs.ncku.edu.tw>
2022-12-05 05:23:58 +0800

    [Finish some function]
    >Finish "show recent activities" function (with related links).

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from crawler import getInfoTitle
+from crawler import getInfoTitle, getActivityUrl
 
 app = Flask(__name__)
 
@@ -12,6 +12,7 @@ linebot_api = LineBotApi('aWTrEwZVA3XLmWzRnyYM0akO2YA8pkbROsAtAI69ZENz0VwF26vRel
 # Channel secret
 handler = WebhookHandler('e0c3a441bef0e556f6f5717f0b61bbd4')
 
+urlPkgHomePage = 'https://pokemongolive.com'
 urlPkgNewsPage = 'https://pokemongolive.com/news?hl=zh_Hant'
 
 
@@ -33,15 +34,30 @@ def callback():
 def handle_message(event):
     if(event.message.text == '[計算ＩＶ]'):
         linebot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入以下格式：-m 攻擊數值 防禦數值 HP數值'))
+        linebot_api.reply_message(event.reply_token, TextSendMessage(text='1'))
+        # linebot_api.reply_message(event.reply_token, TextSendMessage(text='2'))
+        # linebot_api.reply_message(event.reply_token, TextSendMessage(text='3'))
+
+        # if(event.message.text):
+        #     cmd = event.message.text
+        #     if "-m" in cmd :
+        #         linebot_api.reply_message(event.reply_token, TextSendMessage(text='-m'))
+
     elif(event.message.text == '[最近活動]'):
-        linebot_api.reply_message(event.reply_token, TextMessage(text='最近的活動如下：'))
-
         titles = getInfoTitle(urlPkgNewsPage)
+        blocks = getActivityUrl(urlPkgNewsPage)
 
-        
+        titles_in_string_format = "⌛【Activities】⌛"
 
+        index=0
+        # {index+1}.
         for title in titles[:3]:
-            linebot_api.reply_message(event.reply_token, TextMessage(text='x'))
+            titles_in_string_format += f'{title.string}{urlPkgHomePage}{blocks[index]["href"]}\n'
+            # titles_in_string_format += '==========================='
+            titles_in_string_format += '==-==-==-==-==-==-==-==-==-'
+            index += 1
+
+        linebot_api.reply_message(event.reply_token, TextMessage(text=titles_in_string_format))
 
     elif(event.message.text == '[連結地圖]'):
         linebot_api.reply_message(event.reply_token, TextMessage(text='請將裝置的定位開啟'))
@@ -57,3 +73,6 @@ def handle_message(event):
 
 if __name__ == '__main__':
     app.run()
+
+
+    # linebot_api.reply_message(event.reply_token, TextSendMessage(text=''))
--- crawler.py
+++ crawler.py
@@ -1,9 +1,9 @@
-import requests, bs4
+import requests, bs4 
 
 
 
 urlPkgNewsPage = 'https://pokemongolive.com/news?hl=zh_Hant'
-
+urlPkgHomePage = 'https://pokemongolive.com'
 
 def getInfoTitle(url):
     requestGet = requests.get(url)
@@ -14,4 +14,17 @@ def getInfoTitle(url):
     return titles
         
 
-getInfoTitle(urlPkgNewsPage)
+def getActivityUrl(url):
+    requestGet = requests.get(urlPkgNewsPage)
+    html = bs4.BeautifulSoup(requestGet.text, "html.parser")
+
+    blocks = html.find_all("a", class_='blogList__post')
+
+    return blocks
+
+# url = f'{urlPkgHomePage}{block["href"]}'
+
+
+# for block in blocks[:3]:
+#     url = f'{urlPkgHomePage}{block["href"]}'
+#     print(url)
