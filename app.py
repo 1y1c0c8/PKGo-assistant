cdd56bf709f4f8b45d6545fa1a35110ad0c2417b
1y1c0c8 <f74096255@gs.ncku.edu.tw>
2022-12-16 22:50:05 +0800

    Add: Using dotenv to store sensitive info now.

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
+import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent
+from linebot.models import TextMessage, TextSendMessage
+from linebot.models import LocationMessage, LocationSendMessage
+from linebot.models import ImageMessage, ImageSendMessage
+from linebot.models import VideoMessage, VideoSendMessage
 
+# For crawler
 from crawler import getInfoTitle, getActivityUrl
+# For GPS
+from geopy.geocoders import Nominatim
+# # For OpenAI
+# import openai
+# For .env
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+load_dotenv()
+token = os.getenv("channel_access_token")
+secret = os.getenv("channel_secret")
 # Chennel access token
-linebot_api = LineBotApi('aWTrEwZVA3XLmWzRnyYM0akO2YA8pkbROsAtAI69ZENz0VwF26vRel1b7PAPs5GaaU5+fe70YHFBqi31PE0Q3hXFTGtHC1FuXsyKYfOQ3JI/xyip3OUjrjhubSrgk5gyiAYf7cLIadEQ7wQc0iQFNQdB04t89/1O/w1cDnyilFU=')
+linebot_api = LineBotApi(token)
 # Channel secret
-handler = WebhookHandler('e0c3a441bef0e556f6f5717f0b61bbd4')
+handler = WebhookHandler(secret)
+
 
 urlPkgHomePage = 'https://pokemongolive.com'
 urlPkgNewsPage = 'https://pokemongolive.com/news?hl=zh_Hant'
@@ -32,6 +48,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    UserId = str(event.source.user_id)
+    print(UserId)
+
+
     if(event.message.text == '[計算ＩＶ]'):
         linebot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入以下格式：-m 攻擊數值 防禦數值 HP數值'))
         linebot_api.reply_message(event.reply_token, TextSendMessage(text='1'))
@@ -60,19 +80,43 @@ def handle_message(event):
         linebot_api.reply_message(event.reply_token, TextMessage(text=titles_in_string_format))
 
     elif(event.message.text == '[連結地圖]'):
-        linebot_api.reply_message(event.reply_token, TextMessage(text='請將裝置的定位📍開啟'))
-        
-        # Check GPS
+        linebot_api.reply_message(event.reply_token, TextMessage(text='請發送位置資訊📍'))
+    elif(event.message.text == "測試訊息"):
+        linebot_api.reply_message(event.reply_token, TextMessage(text='做動ing'))
+        # linebot_api.reply_message(event.reply_token, TextMessage(text='可以一次傳兩則訊息'))
+    elif(event.message.text == "甩頭鸚鵡"):
+        # image_message = ImageSendMessage(
+        #     original_content_url= "https://imgur.com/gallery/cf8cZdA",
+        #     preview_image_url= "https://imgur.com/gallery/cf8cZdA"
+        # )
+        video_message = VideoSendMessage(
+            original_content_url= "https://youtu.be/f67WbPVPIWc",
+            preview_image_url= "https://youtu.be/f67WbPVPIWc"
+        )
+        linebot_api.reply_message(event.reply_token, video_message)
 
 
 
-        # linebot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    if event.type == 'message':
+        if event.message.type == 'location':
+            UserId = str(event.source.user_id)
+            print(UserId)
+
+            geolocator = Nominatim(user_agent=f'{UserId}')
+            
+            location = geolocator.reverse(f'{event.message.latitude}, {event.message.longitude}')
+            linebot_api.reply_message(event.reply_token, TextSendMessage(
+                text=f'Get location message!\nYour User ID is [ {UserId} ]\n==-==-==-==-==-==-==-==-==-==-==\nCurrent location:\n{location.address}\n==-==-==-==-==-==-==-==-==-==-==\n{round(location.latitude, 6)}, {round(location.longitude, 6)}\n==-==-==-==-==-==-==-==-==-==-=='))
+
+            # linebot_api.reply_message(event.reply_token, TextSendMessage(text=f'http://www.google.com/maps/place/{event.message.latitude},{event.message.longitude}'))
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="localhost", port=8000)
 
 
     # linebot_api.reply_message(event.reply_token, TextSendMessage(text=''))
