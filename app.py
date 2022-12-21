c826cee4dd3689ab23cd45f27857602668af0282
1y1c0c8 <f74096255@gs.ncku.edu.tw>
2022-12-22 00:27:55 +0800

    補push

--- app.py
+++ app.py
@@ -9,19 +9,25 @@ from linebot.models import ImageMessage, ImageSendMessage
 from linebot.models import VideoMessage, VideoSendMessage
 
 # For crawler
-from crawler import getInfoTitle, getActivityUrl
+from crawler import getInfoTitle, getActivityUrl, mergeInfo
 # For GPS
 from geopy.geocoders import Nominatim
 # # For OpenAI
 # import openai
 # For .env
 from dotenv import load_dotenv
+# For expression
+import re
+
+from pkmgo_assistant import appraise as aps
+from pkmgo_assistant import crawler
 
 app = Flask(__name__)
 
 load_dotenv()
 token = os.getenv("channel_access_token")
 secret = os.getenv("channel_secret")
+project_url = os.getenv("project_url")
 # Chennel access token
 linebot_api = LineBotApi(token)
 # Channel secret
@@ -30,6 +36,10 @@ handler = WebhookHandler(secret)
 
 urlPkgHomePage = 'https://pokemongolive.com'
 urlPkgNewsPage = 'https://pokemongolive.com/news?hl=zh_Hant'
+urlPkgTrainerClubHomePage = "https://pogotrainer.club"
+urlPkgTrainerClubWorldwidePage = "https://pogotrainer.club/?sort=worldwide"
+
+appraise_pattern = r'[0-9]{1,2}\s[0-9]{1,2}\s[0-9]{1,2}'
 
 
 # @app.route('/')
@@ -49,23 +59,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     UserId = str(event.source.user_id)
-    print(UserId)
-
-
+    # print(UserId)
     if(event.message.text == '[計算ＩＶ]'):
-        linebot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入以下格式：-m 攻擊數值 防禦數值 HP數值'))
-        linebot_api.reply_message(event.reply_token, TextSendMessage(text='1'))
-        # linebot_api.reply_message(event.reply_token, TextSendMessage(text='2'))
-        # linebot_api.reply_message(event.reply_token, TextSendMessage(text='3'))
-
-        # if(event.message.text):
-        #     cmd = event.message.text
-        #     if "-m" in cmd :
-        #         linebot_api.reply_message(event.reply_token, TextSendMessage(text='-m'))
-
+        linebot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入以下格式：攻擊數值 防禦數值 HP數值'))
+    elif(re.fullmatch(appraise_pattern, event.message.text)):
+        tokens = event.message.text.split()
+        pkm_iv = aps.aps_compute(tokens)
+        star = aps.star_check(pkm_iv)
+        linebot_api.reply_message(event.reply_token, TextSendMessage(text = f'This pokemn\'s IV is {star} {pkm_iv} !'))
     elif(event.message.text == '[最近活動]'):
-        titles = getInfoTitle(urlPkgNewsPage)
-        blocks = getActivityUrl(urlPkgNewsPage)
+        titles = crawler.getInfoTitle(urlPkgNewsPage)
+        blocks = crawler.getActivityUrl(urlPkgNewsPage)
 
         titles_in_string_format = "⌛【Activities】⌛"
 
@@ -73,7 +77,6 @@ def handle_message(event):
         # {index+1}.
         for title in titles[:3]:
             titles_in_string_format += f'{title.string}{urlPkgHomePage}{blocks[index]["href"]}\n'
-            # titles_in_string_format += '==========================='
             titles_in_string_format += '==-==-==-==-==-==-==-==-==-==-=='
             index += 1
 
@@ -81,20 +84,24 @@ def handle_message(event):
 
     elif(event.message.text == '[連結地圖]'):
         linebot_api.reply_message(event.reply_token, TextMessage(text='請發送位置資訊📍'))
-    elif(event.message.text == "測試訊息"):
+    elif(event.message.text == "加訓練師好友"):
+        add_friends = TextSendMessage(text = mergeInfo())
+        linebot_api.reply_message(event.reply_token, add_friends)
+    elif(event.message.text == "[Video]"):
+        Me_at_the_zoo = VideoSendMessage(
+            original_content_url= project_url + "/static/Me_at_the_zoo.mp4",
+            preview_image_url= project_url + "/static/Me_at_the_zoo.png"
+        )
+        linebot_api.reply_message(event.reply_token, Me_at_the_zoo)
+        # txt = TextSendMessage(text= str(project_url+"/static/Me_at_the_zoo.mp4"))
+        # linebot_api.reply_message(event.reply_token, txt)
+    elif(event.message.text == "[Creater Info]"):
+        linktr = TextSendMessage(text="https://linktr.ee/hoffffoh")
+        linebot_api.reply_message(event.reply_token, linktr)
+    elif(event.message.text == "wake up it's hoff on the board"):
         linebot_api.reply_message(event.reply_token, TextMessage(text='做動ing'))
         # linebot_api.reply_message(event.reply_token, TextMessage(text='可以一次傳兩則訊息'))
-    elif(event.message.text == "甩頭鸚鵡"):
-        # image_message = ImageSendMessage(
-        #     original_content_url= "https://imgur.com/gallery/cf8cZdA",
-        #     preview_image_url= "https://imgur.com/gallery/cf8cZdA"
-        # )
-        video_message = VideoSendMessage(
-            original_content_url= "https://youtu.be/f67WbPVPIWc",
-            preview_image_url= "https://youtu.be/f67WbPVPIWc"
-        )
-        linebot_api.reply_message(event.reply_token, video_message)
-
+    
 
 
 @handler.add(MessageEvent, message=LocationMessage)
@@ -109,7 +116,7 @@ def handle_location_message(event):
             location = geolocator.reverse(f'{event.message.latitude}, {event.message.longitude}')
             linebot_api.reply_message(event.reply_token, TextSendMessage(
                 text=f'Get location message!\nYour User ID is [ {UserId} ]\n==-==-==-==-==-==-==-==-==-==-==\nCurrent location:\n{location.address}\n==-==-==-==-==-==-==-==-==-==-==\n{round(location.latitude, 6)}, {round(location.longitude, 6)}\n==-==-==-==-==-==-==-==-==-==-=='))
-
+        
             # linebot_api.reply_message(event.reply_token, TextSendMessage(text=f'http://www.google.com/maps/place/{event.message.latitude},{event.message.longitude}'))
 
 
